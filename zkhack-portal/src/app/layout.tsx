4bcfcda517a6b8c8379b38cfbf3a12418f3adf45
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2025-06-21 20:35:15 +0200

    add favicon

--- zkhack-portal/src/app/layout.tsx
+++ zkhack-portal/src/app/layout.tsx
@@ -3,8 +3,14 @@ import "./globals.css";
 import { GameProvider } from "./context/GameProvider";
 
 export const metadata: Metadata = {
-  title: "ZKHack",
-  description: "ZKHack Berlin",
+  title: "Kebab Quest - ZKHack",
+  description:
+    "Navigate through mazes and collect kebabs in this ZK-powered game",
+  icons: {
+    icon: "/icon.svg",
+    shortcut: "/icon.svg",
+    apple: "/icon.svg",
+  },
 };
 
 export default function RootLayout({
@@ -14,10 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/icon.svg" type="image/svg+xml" />
+      </head>
       <body className="">
-        <GameProvider>
-          {children}
-        </GameProvider>
+        <GameProvider>{children}</GameProvider>
       </body>
     </html>
   );
